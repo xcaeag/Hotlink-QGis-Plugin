6023003c339ccc0c04184cf4af343fde6e0a072a
Peter Petrik <zilolv@gmail.com>
2016-04-19 10:31:09 +0200

    fix #1 Support for QGIS expression for hotlinks

--- Hotlink/HotlinkMT.py
+++ Hotlink/HotlinkMT.py
@@ -18,11 +18,12 @@ class HotlinkMT(QgsMapTool):
     """
 
     def __init__(self, plugin):
-        """Tool initialization 
+        """Tool initialization
         """
         QgsMapTool.__init__(self, plugin.canvas)
 
         # specifics initializations
+        self.canvas = plugin.canvas
         self.plugin = plugin
         self.featuresFound = []
         self.ixFeature = 0
@@ -30,20 +31,38 @@ class HotlinkMT(QgsMapTool):
         self.chooserDlg = None
         self.request = QgsFeatureRequest()
         self.request.setFlags(QgsFeatureRequest.Flags(QgsFeatureRequest.NoGeometry | QgsFeatureRequest.ExactIntersect))
-        
+
     def canvasPressEvent(self, event):
         pass
+
+    def _layer_tooltip(self, layer, feat):
+        tooltip = layer.name()
+        df = layer.displayField()
+        idx = layer.fieldNameIndex( df )
+        if idx < 0:
+            context = QgsExpressionContext()
+            context.appendScope(QgsExpressionContextUtils.globalScope())
+            context.appendScope(QgsExpressionContextUtils.projectScope())
+            context.appendScope(QgsExpressionContextUtils.layerScope(layer))
+            context.appendScope( QgsExpressionContextUtils.mapSettingsScope( self.canvas.mapSettings() ) )
+            context.setFeature( feat )
+
+            tooltip += " - " + QgsExpression.replaceExpressionText( df, context )
+        else:
+            tooltip += " - " + feat.attribute(df)
+
+        return tooltip
 
     def findUnderlyingObjects(self, event, saveFeatures):
         """On mouse movement, we identify the underlying objects
         """
-        
+
         if not self.plugin.active:
             return
-        
+
         try:
             self.__pos = event.pos()
-            
+
             # find objects
             features = self._getFeatures()
 
@@ -55,9 +74,9 @@ class HotlinkMT(QgsMapTool):
                 # build a list of tuples Name / feature / layer / id for construction of the tool tip, the interface of choice
                 if saveFeatures:
                     self.featuresFound = [ {"actionName":QtGui.QApplication.translate("aeag_search", "Choose...", None, QtGui.QApplication.UnicodeUTF8), "feature":None, "layer":None, "idxAction":None} ]
-                    
+
                 tooltip = []
-             
+
                 for featData in features:
                     feat = featData["feature"]
                     layer = featData["layer"]
@@ -69,17 +88,11 @@ class HotlinkMT(QgsMapTool):
                                 actionName = action.name()
                         except:
                             actionName = action.name()
-                            
+
                         if saveFeatures:
                             self.featuresFound.append( {"actionName":"    "+actionName, "feature":feat, "layer":layer, "idxAction":idxAction} )
-                            
-                    try:
-                        if layer.displayField() and feat.attribute(layer.displayField()):
-                            tooltip.append(layer.name() + " - " + feat.attribute(layer.displayField()))
-                        else:
-                            tooltip.append(layer.name())
-                    except:
-                        tooltip.append(layer.name())
+
+                        tooltip.append(self._layer_tooltip(layer, feat))
 
                 # display
                 self.plugin.canvas.setToolTip('\n'.join(tooltip))
@@ -88,7 +101,7 @@ class HotlinkMT(QgsMapTool):
                 # without objects, restore the cursor ...
                 if saveFeatures:
                     self.featuresFound = []
-                    
+
                 self.plugin.canvas.setCursor(QCursor(Qt.ArrowCursor))
                 self.plugin.canvas.setToolTip("")
         except:
@@ -98,7 +111,7 @@ class HotlinkMT(QgsMapTool):
         """On mouse movement, we identify the underlying objects
         """
         self.findUnderlyingObjects(event, False)
-            
+
     def canvasDoubleClickEvent(self, event):
         pass
 
@@ -113,7 +126,7 @@ class HotlinkMT(QgsMapTool):
         """
         if not self.plugin.active:
             return
-        
+
         # left click only
         if event.button() not in (Qt.LeftButton, Qt.RightButton):
             return
@@ -122,14 +135,14 @@ class HotlinkMT(QgsMapTool):
 
         # if a single action (2 lines in the list)
         #QgsMessageLog.logMessage("Nb "+str(len(self.featuresFound)), 'Extensions')
-        
+
         if len(self.featuresFound) == 2:
             layer = self.featuresFound[1]["layer"]
             idx = self.featuresFound[1]["idxAction"]
             feature = self.featuresFound[1]["feature"]
-            
+
             self.doAction(layer, idx, feature)
-            
+
         else:
             # to choose the action to trigger
             canvasPos = self.plugin.canvas.mapToGlobal(QPoint(0,0))
@@ -138,16 +151,16 @@ class HotlinkMT(QgsMapTool):
 
     def activate(self):
         pass
-     
+
     def deactivate(self):
         pass
-        
+
     def doAction(self, layer, idx, feature):
         if layer.actions().at(idx).action() == 'openFeatureForm':
             self.plugin.iface.openFeatureForm(layer, feature)
-        else :  
+        else :
             layer.actions().doActionFeature(idx, feature)
-        
+
     def onChoose(self, idx):
         """Do action
         """
@@ -157,20 +170,22 @@ class HotlinkMT(QgsMapTool):
     def _getFeatures(self):
         """Identify objects under the mouse, having actions
         """
+        searchRadius = self.searchRadiusMU(self.canvas)
+        point = self.toMapCoordinates(self.__pos)
+
         features = []
-        
-        transform = self.plugin.canvas.getCoordinateTransform()
-        ll = transform.toMapCoordinates( self.__pos.x()-3, self.__pos.y()+3 )
-        ur = transform.toMapCoordinates( self.__pos.x()+3, self.__pos.y()-3)
-        selectRect =  QgsRectangle (ll.x(), ll.y(), ur.x(), ur.y())
-        rectGeom = QgsGeometry.fromRect(selectRect)
 
         for layer in self.plugin.canvas.layers():
             # treat only vector layers having actions
-            if layer.type() == QgsMapLayer.VectorLayer and layer.actions().size() > 0:                
-                # selection (bbox intersections)
-                self.request.setFilterRect(selectRect)
+            if layer.type() == QgsMapLayer.VectorLayer and layer.actions().size() > 0:
+                rect = QgsRectangle()
+                rect.setXMinimum(point.x() - searchRadius)
+                rect.setXMaximum(point.x() + searchRadius)
+                rect.setYMinimum(point.y() - searchRadius)
+                rect.setYMaximum(point.y() + searchRadius)
+                rect = self.toLayerCoordinates(layer, rect)
+                self.request.setFilterRect(rect)
                 for feature in layer.getFeatures(self.request):
                     features.append({"layer":layer, "feature":feature})
-            
+
         return features
